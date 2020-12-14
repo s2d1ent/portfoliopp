93e6db93b2486a00ac97709c095affd73951978b
s2d1ent <vornfrost@gmail.com>
2020-12-15 02:31:58 +0300

    v0.1

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+ <title> S2d1ent portfolio </title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style/index.css">
+<link rel="shortcut icon" href="src/s2dLogo.png">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=1536">
+
+</head>
+<body id="body">
+	<header id="header">
+		<a href="index.php"><img src="src/s2dLogo.png" class="logo"></a>
+		<nav id="nav">
+			<a href="#" target="_blank" class="nav-a">Главная</a>
+			<a href="#" target="_blank" class="nav-a">Макеты</a>
+			<a href="#" target="_blank" class="nav-a">Сделать заказ</a>
+		</nav>
+
+		<div class="signin">
+			<a href="signin.php" class="signin__link">signin</a>
+		</div>
+
+	</header>
+<section class="about">
+	<dir class="about__container">
+		<div class="about__left">
+			<img src="src/s2dLogo.png" class="about__container-img">
+			<h2 class="about__container-name">s2d1ent</h2>
+			<div class="about__container-subtitle">Web-dev</div>
+		</div>
+		<div class="about__info-rel">
+			<h2 class="about__info-title">Имя</h2>
+			<div class="about__info-subtitle">Виктор</div>
+			<h2 class="about__info-title">Возраст</h2>
+			<div class="about__info-subtitle">16</div>
+		</div>
+		<div class="about__right">
+			<h2 class="about__right-title">Мои контакты</h2>
+			<ul class="about__contacts">
+				<li class="about__contacts-li">
+					<a target="_blank" href="https://vk.com/vornfrosti" class="about__contacts-li">VK
+					</a>
+				</li>
+				<li class="about__contacts-li">
+					<a target="_blank" href="https://twitter.com/s2d1ent" class="about__contacts-li">Twitter
+					</a>
+				</li>
+				<li class="about__contacts-li">
+					<a target="_blank" href="https://github.com/s2d1ent" class="about__contacts-li">GitHub
+					</a>
+				</li>
+				<li class="about__contacts-li">tumenev33@mail.ru</li>
+				<li class="about__contacts-li">vornfrost@gmail.com</li>
+			</ul>
+		</div>
+		<div class="about__info">
+			<h2 class="about__info-title">Стаж</h2>
+			<div class="about__info-subtitle">1 год</div>
+			<h2 class="about__info-title">Языки</h2>
+			<div class="about__info-subtitle">HTML, CSS, JS, PHP</div>
+			<h2 class="about__info-title">Проекты</h2>
+			<div class="about__info-subtitle">3</div>
+		</div>
+	</dir>
+</section>
+<section class="proj">
+	<div class="proj__container">
+		<h2 class="proj__container-title"> Мои проекты</h2>
+		<div class="proj__table">
+			<div class="proj__table-left">
+				<img src="" class="proj__table-img">
+			</div>
+			<div class="proj__table-right">
+				<h2 class="proj__table-title"></h2>
+				<div class="proj__table-subtitle"></div>
+			</div>
+		</div>
+	</div>
+</section>
+<script type="text/javascript" async src="scripts/changecolor.js"></script>
+<script type="text/javascript" async src="scripts/index.js"></script>
+</body>
+</html>
